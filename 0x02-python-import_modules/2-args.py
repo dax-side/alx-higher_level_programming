66c125c753f99d7fb0b1475a8f5a30a5cbac86f6
dax-side <damade615@gmail.com>
2022-06-30 14:13:19 -0700

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
     import sys
 
     print("{:d} argument".format(len(sys.argv) - 1), end="")
-    if len(sys.argv) == 1:
-        print(".")
-    elif len(sys.argv) == 2:
-        print(":")
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument:".format(num - 1))
     else:
-        print("s:")
+        print("{} arguments:".format(num - 1))
 
-    for i in range(len(sys.argv) - 1):
-        print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
+    for i in range(1, num):
+        print("{}: {}".format(i, sys.argv[i]))
