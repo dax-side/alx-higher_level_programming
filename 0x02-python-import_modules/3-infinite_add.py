4e5d828fec788ca1aad2e20dc455875159718164
dax-side <damolaadegbite77@gmail..com>
2022-06-30 21:33:58 +0000

    new

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    result = 0
-    for i in sys.argv:
-        if i != sys.argv[0]:
-            result += int(i)
-            print(result)
