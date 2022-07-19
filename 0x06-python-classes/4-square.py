9eaa5c8e88538ea137738bd3ebfc546eeb964b38
dax-side <damade615@gmail.com>
2022-07-19 19:38:19 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module that includes Square class"""
+"""Module that includes Square class """
 
 
 class Square:
-    """defines a square object"""
+    """defines a square class"""
     def __init__(self, size=0):
         """initializes the size"""
         self.__size = size
@@ -14,5 +14,18 @@ class Square:
             raise TypeError("size must be an integer")
 
     def area(self):
-        """calculates and returns the current area"""
-        return self.__size * self.__size
+        """returns the current area """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """ gets the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Method that sets the new size of the square"""
+        if type(value) == int:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
