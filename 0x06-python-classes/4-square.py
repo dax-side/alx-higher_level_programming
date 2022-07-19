1254962040ca78c949691664d3b13bd688f000b5
dax-side <damade615@gmail.com>
2022-07-19 19:53:12 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""Module that includes Square class """
+"""
+This is the "Square"  module.
+
+This module provides a simple Square class with initialize size.
+Defaults size to 0. Raise error on invalid size inputs.
+Methods Getter and Setter properties for size.
+Method area returns size of area of the square.
+"""
 
 
 class Square:
-    """defines a square class"""
+    """A class that defines a square by size and can compute area"""
     def __init__(self, size=0):
-        """initializes the size"""
-        self.__size = size
-        if type(size) == int:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-        else:
-            raise TypeError("size must be an integer")
-
-    def area(self):
-        """returns the current area """
-        return (self.__size ** 2)
+        self.size = size
 
     @property
     def size(self):
-        """ gets the size of the square"""
         return self.__size
 
     @size.setter
-    def size(self, value):
-        """ Method that sets the new size of the square"""
-        if type(value) == int:
-                self.__size = value
-        else:
+    def size(self, size):
+        if type(size) != int:
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
